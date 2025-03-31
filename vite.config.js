8a703fefa499f7931420bede76c8cef9453580a7
MSulSal <msulsaleem@gmail.com>
2025-03-31 17:32:41 -0500

    set up github actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Set base to your repository name (including leading and trailing slash)
+  base: "/random-walks/",
+});
