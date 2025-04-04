46d5ac4a52c04a109b74d11e358efa805a2074da
MSulSal <msulsaleem@gmail.com>
2025-04-03 19:02:56 -0500

    fix refresh

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,31 @@ const App = () => {
       <div className="canvas-container">
         <p>Simple Random Walk</p>
         <RandomWalk />
+      </div>
+      <div className="canvas-container">
         <p>Random Walk with Diagonal Steps</p>
         <RandomWalkDiagonal />
+      </div>
+      <div className="canvas-container">
         <p>Random Walk with Continuous Range in Step Sizes</p>
         <RandomWalkContinuous />
+      </div>
+      <div className="canvas-container">
         <p>
           Random Walk with Random Colors, Larger and Variable Step Size Range,
           Stroke Width, and Reflection
         </p>
         <RandomWalkColorful />
+      </div>
+      <div className="canvas-container">
         <p>Random Walk with greater tendency to go right and downwards</p>
         <RandomWalkSouthEast />
+      </div>
+      <div className="canvas-container">
+        <p>Random Walk with 50% chance of following cursor</p>
+        <RandomWalkMouse />
+      </div>
+      <div className="canvas-container">
         <p>Random Walk with 50% chance of following cursor</p>
         <RandomWalkMouse />
       </div>
