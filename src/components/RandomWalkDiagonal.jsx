08ac7edaa5d5b904ee73132b2b476bf606dd2d58
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-08-13 11:01:50 -0500

    fix diagonals

--- src/components/RandomWalkDiagonal.jsx
+++ src/components/RandomWalkDiagonal.jsx
@@ -12,11 +12,11 @@ class RandomWalker {
     let ystep = this.p5.floor(this.p5.random(3)) - 1;
     this.x +=
       this.x + xstep * 10 > 0 && this.x + xstep * 10 < this.p5.width
-        ? this.x + xstep * 10
+        ? xstep * 10
         : 0;
     this.y +=
       this.y + ystep * 10 > 0 && this.y + ystep * 10 < this.p5.height
-        ? this.y + ystep * 10
+        ? ystep * 10
         : 0;
   }
 
